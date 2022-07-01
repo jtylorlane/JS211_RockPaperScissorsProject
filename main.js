36a0e9482a4549981e2b8351e3a81a19d06011ef
jtylorlane <jtylorlane@yahoo.com>
2022-07-01 17:24:42 -0500

    changes

--- main.js
+++ main.js
@@ -14,10 +14,56 @@ const rl = readline.createInterface({
 // the function that will be called by the unit test below
 const rockPaperScissors = (hand1, hand2) => {
 
-  // Write code here
-  // Use the unit test to see what is expected
+ 
 
-}
+//should scrub input to ensure lowercase with "trim"ed whitepace
+
+hand1 = hand1.toLowerCase()
+hand2 = hand2.toLowerCase()
+
+hand1 =hand1.trim()
+hand2 =hand2.trim()
+
+// 1. Compare User1 input to User2 input.
+
+if ( (hand1 == 'rock' && hand2== 'rock') || (hand1 == 'paper' && hand2== 'paper') || (hand1 == 'scissors' && hand2== 'scissors') ) {
+  // it('should detect a tie', () => {
+// 1. If User1 input is 'rock' and User2 input is 'rock', it's a tie.
+// 1. If User1 input is 'paper' and User2 input is 'paper', it's a tie.
+// 1. If User1 input is 'scissors' and User2 input is 'scissors', it's a tie.
+  // console.log("It's a tie!")
+
+  return "It's a tie!"
+} else if ((hand1 == 'rock' && hand2== 'scissors') || (hand1 == 'paper' && hand2== 'rock') || (hand1 == 'scissors' && hand2== 'paper') ) {
+
+
+// it('should detect which hand won' hand1 wins
+// 1. If User1 input is 'rock' and User2 input is 'scissors', User1 wins.
+// 1. If User1 input is 'paper' and User2 input is 'rock', User1 wins.
+// 1. If User1 input is 'scissors' and User2 input is 'paper', User1 wins.
+
+// console.log("Hand one wins!")
+
+  return "Hand one wins!"
+} else if  ((hand1 == 'rock' && hand2== 'paper') || (hand1 == 'paper' && hand2== 'scissors') || (hand1 == 'scissors' && hand2== 'rock') ) {
+
+
+  // it('should detect which hand won' hand2 wins
+ // 1. If User1 input is 'rock' and User2 input is 'paper', User2 wins.
+// 1. If User1 input is 'paper' and User2 input is 'scissors', User2 wins.
+// 1. If User1 input is 'scissors' and User2 input is 'rock', User2 wins.
+  
+  // console.log("Hand two wins!")
+
+  return "Hand two wins!"
+
+  } else { 
+    
+    getPrompt()
+
+  }
+}// end of function
+
 
 // the first function called in the program to get an input from the user
 // to run the function use the command: node main.js
